95def561d8e94e73ae34f6129795b8d49e9b13ee
Alberto Valtierra Jr <betomasia12@gmail.com>
2023-01-28 01:55:14 +0000

    finished front end of the project, need to finished backend project

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,7 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+import hashlib
 
 api = Blueprint('api', __name__)
 
@@ -16,3 +17,17 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/createaccount', methods=['POST'])
+def create_account():
+    body = request.get_json(force = True)
+    email = body['email']
+    password = str(hashlib.sha256(body['password'].encode()))
+    has_email = User.query.filter_by(email = email).first()
+    if has_email is None:
+        new_user = User(email = email, password = password, is_active = True)
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify('Successfully created account!')
+    else:
+        return 'User already exists :('
